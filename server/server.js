b68a71e156e019b9d2b20e5dae1e9fdac332127e
gzeal <unovan@protonmail.com>
2024-01-10 21:33:01 -0500

    added pseudocode for tasks to work on

---
+++ server/server.js
@@ -0,0 +1,52 @@
+// import for environmental variables - .config => when you type it autofills
+require('dotenv').config();
+
+const URI = `mongodb+srv://${db_userName}:${db_password}@axolotl.xogzh1q.mongodb.net/?retryWrites=true&w=majority`;
+
+                                                // * = working on now
+
+
+// take in json obj and observe static files
+
+// routes for landing page -- localhost?
+
+// login and sign up logic
+    //app.post logic etc
+
+// account creation logic *
+
+// logic to test mongoose connection (connected to database) *
+  //error handling
+
+// functionality to start the server
+
+// root for adding a drink *
+
+// route for deleting a drink
+
+// edit/update a drink
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// extras: 
+
+// sessions/cookies for down the line
+// bcrypt addition
