9a487015b572d1f4a7f16b376a9557e0aad5e805
Josh <josh.r.bajarias@gmail.com>
2024-01-10 22:00:03 -0500

    wrote mongoose connection logic to verify connectivity

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
-// test
+// allows us to use 'mongoose' functionality
+const mongoose = require('mongoose');
+
+// logic that will display in terminal if mongoose connection to atlas is successful
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected to Atlas!')
+});
+
+// logic that will display in terminal if mongoose to atlas encounters an error
+mongoose.connection.on('error', (err) => {
+    console.log('Mongoose connection error: ', err)
+});
+
+
+// logic that will display in terminal if mongoose to atlas is disconnected
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected.')
+});
