59c97070a03e5db1ef5783f2b735f0bc088b747b
LNRtopher <143825175+LNRtopher@users.noreply.github.com>
2024-01-11 20:06:55 -0800

    "merge from dev"

--- server/server.js
+++ server/server.js
@@ -34,13 +34,7 @@ mongoose.connection.on('disconnected', () => {
 });const express = require('express');
 
 
-
-
-
-
-
-
-
+                                                // * = working on now
 
 
 // take in json obj and observe static files
@@ -62,6 +56,121 @@ app.post('/login', userController.verifyUser, cookieController.setSSIDCookie, se
     }
     else {res.status(401).send('problem with username and/or password')};
 });
+
+// functionality to start the server
+
+// route for adding a drink *      // similar logic for when I make a branch const addDrink = new drinkSchema({})
+  
+// middleware for validating drinks
+
+drinkDataValidation = (req, res, next) => {
+// confirm required data
+
+// error status if incorrect
+
+}
+
+app.post('/addDrink', drinkDataValidation, (req, res) => {
+// add a res 200, possibly try and catch blocks
+  res.json({ success: true, message: 'Drink was added to the database' });
+});
+
+// route for deleting a drink
+
+// edit/update a drink
+  //error handling
+
+// server: 
+app.listen(PORT, ()=>{ console.log(`Listening on port ${PORT}...`); });
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// extras: 
+
+// sessions/cookies for down the line
+// bcrypt addition
+// allows us to use 'mongoose' functionality
+const mongoose = require('mongoose');
+
+const app = express();
+// import for environmental variables - .config => when you type it autofills
+require('dotenv').config();
+
+const URI = `mongodb+srv://${db_userName}:${db_password}@axolotl.xogzh1q.mongodb.net/?retryWrites=true&w=majority`;
+
+// route for creating new user:
+
+
+// logic that will display in terminal if mongoose connection to atlas is successful
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected to Atlas!')
+});
+
+// logic that will display in terminal if mongoose to atlas encounters an error
+mongoose.connection.on('error', (err) => {
+    console.log('Mongoose connection error: ', err)
+});
+
+
+// logic that will display in terminal if mongoose to atlas is disconnected
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected.')
+});const express = require('express');
+
+
+
+
+
+
+
+
+
+
+
+// take in json obj and observe static files
+
+// routes for landing page -- localhost?
+
+const userController = require('./controllers/userController');
+// route for creating new user:
+app.post('/signup', userController.createUser, (req, res)  => {
+    console.log('new user created')
+    res.status(201).json(res.locals.userID);
+});
+
+// login and sign up logic
+app.post('/login', userController.verifyUser, (req, res) => {
+    if(res.locals.verifiedUser) {
+    console.log('found user in db')
+    res.status(201).json(res.locals.userID)
+    }
+    else res.status(401).json(userID);
+});
+
+// account creation logic *
+
+// logic to test mongoose connection (connected to database) *
+  //error handling
 
 // functionality to start the server
 
