54cdfc2b1cf79d1e11382beb66c6b24f2fafc442
Andy Guajardo <andyguajardo@mac-mini.lan>
2024-01-11 19:09:34 -0800

    barebones for feed

--- client/components/Test.jsx
+++ client/components/Test.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Test = () => {
   return (
     
-    <div style={{background:'red'}}>You have arrived at test component 
+    <div style={{background:'red',height:'100vh',textAlign:'center'}}>
+      <h1>
+        You have arrived at test component, this was made to test React router was working.
+        </h1>
+        <br></br>
+        <h3> 
+         IM RED AND ANGRY ARGHHHH. 
+        </h3>
       <br></br>
     </div>
   )
