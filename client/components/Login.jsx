2ac503b4fe4157baf597770891d4ec87b49f55d8
RococoKid <sara.r.kalkstein@gmail.com>
2024-01-11 21:59:35 -0500

    login component logic

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -4,11 +4,34 @@ import { createRoot } from 'react-dom/client';
 import {Link, Route, Routes, useNavigate} from "react-router-dom";
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Submitted:', { username, email });
+      };
 
     return(
         <div>
             <form>
-                
+                <label>
+                Username:
+                <input
+                    type='text'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                </label>
+                <br />
+                <label>
+                    Email:
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
             </form>
         </div>
     )
