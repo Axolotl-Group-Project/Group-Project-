6c8615f174a1faab0a830c09096ebcb4dcf5230d
Andy Guajardo <andyguajardo@mac-mini.lan>
2024-01-13 12:22:12 -0800

    tested drinkList w sampleData and working

--- client/components/SingleDrink.jsx
+++ client/components/SingleDrink.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 
 const SingleDrink = (props) => {
-    console.log('props ->', props);
-// const drinkList =
+  console.log('props ->', props);
+  // const drinkList =
 
   return (
-    <div>
-        <ul>
-            <li>Drink Name</li>
-            <li>Location</li>
-            <li>Ingredients</li>
-            <li>Thoughts</li>
-            <li>Recovery Thoughts</li>
-        </ul>
+    <div style={{ display: 'flex', justifyContent: 'space-between', border: 'solid' }}>
+      <ul>
+        <li>Drink Name</li>
+        <li>Location</li>
+        <li>Ingredients</li>
+        <li>Thoughts</li>
+        <li>Recovery Thoughts</li>
+      </ul>
+      <div style={{
+        border: 'solid', height: '120px', width: '120px', marginRight: '50px', marginTop: '15px', backgroundColor: 'white'
+      }}>
+        <p> Image here</p>
+      </div>
     </div>
   )
 }
 
-export default SingleDrink
+export default SingleDrink;
+//this component not needed after all 
--- client/pages/DrinkFeed.jsx
+++ client/pages/DrinkFeed.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 
 import { useState, useEffect } from 'react'
-import SingleDrink from '../components/SingleDrink.jsx';
+// import SingleDrink from '../components/SingleDrink.jsx';
 
 const DrinkFeed = () => {
-    const [search,setSearch] = useState('');
+    const [search, setSearch] = useState('');
     const [drink, setDrink] = useState('');
     const [location, setLocation] = useState('');
     const [ingredients, setIngredients] = useState('');
     const [thoughts, setThoughts] = useState('');
     const [recoveryThoughts, setRecoveryThoughts] = useState('');
 
-    const [drinkList,setDrinkList] = useState([])
+    const [drinkList, setDrinkList] = useState([])
+
+    //for testig purposes while backend is still being set up, this is current drinkList 
+    const [sampleList, setSampleList] = useState([{
+        drink: 'beer',
+        location: 'here',
+        ingredients: 'brown stuff',
+        thoughts: 'it was good',
+        recoveryThoughts: 'not too long'
+    }, {
+        drink: 'wine',
+        location: 'there',
+        ingredients: 'red stuff',
+        thoughts: 'it was splendid ',
+        recoveryThoughts: 'not too long'
+    }, {
+        drink: 'water',
+        location: 'everywhere',
+        ingredients: 'clearstuff',
+        thoughts: 'the best',
+        recoveryThoughts: 'none'
+    }]);
 
     //upon first render of page, this should fetch drink list from DB and set state for drinkList
     // useEffect(()=>{
@@ -35,9 +56,7 @@ const DrinkFeed = () => {
     // },[])
 
 
-
-
-
+    //func that will get relevant data from state, bundle it in body of POST req, and send to DB to make new drink document
     const handleAddDrinkButton = () => {
         const drinkInfo = {
             drink,
@@ -46,6 +65,7 @@ const DrinkFeed = () => {
             thoughts,
             recoveryThoughts
         };
+        //could add logic here to check that all drinkInfo fields have info, if not throw error 
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -53,39 +73,47 @@ const DrinkFeed = () => {
         };
 
         fetch('http://localhost:9000/addDrink', requestOptions) //change DB URL as needed here
-            .then(response =>{
-                if (!response.ok){
-                    throw new Error (
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(
                         `HTTP error: res status -> ${response.status}`
                     );
                 }
                 //logic if you want to do something after posting new drink, navigate somewhere? pop up that confirms added drink?
             })
-            .catch( error =>{
+            .catch(error => {
                 console.log('error from with in fetch req of add drink');
                 console.error(error);
             })
     };
 
-    const drinkTable = drinkList.map(({drink,location,ingredients,thoughts,recoveryThoughts,_id}) =>{
-            <ul>
-                <li>{drink}</li>
-                <li>{location}</li>
-                <li>{ingredients}</li>
-                <li>{thoughts}</li>
-                <li>{recoveryThoughts}</li>
-                <li>{_id}</li>
-            </ul>
+    //can change sampleList to drinkList once fetch reqs work, make sure to add _id into drink list/component
+    const testTable = sampleList.map(({ drink, location, ingredients, thoughts,recoveryThoughts },idx) => { //destrcuture data from drink object
+        return (
+            //change idx to relevant_id given by DB 
+            <div key={idx} style={{ display: 'flex', justifyContent: 'space-between', border: 'solid' }}>
+                <ul >
+                    <li>Drink: {drink}</li>
+                    <li>Location: {location}</li>
+                    <li>Ingredients: {ingredients}</li>
+                    <li>Thoughts: {thoughts}</li>
+                    <li>Recovery Thoughts: {recoveryThoughts}</li>
+                </ul>
+                <div style={{border: 'solid', height: '120px', width: '120px', marginRight: '50px', marginTop: '15px', backgroundColor: 'white'}}
+                >
+                    <p>Image here eventually</p>
+                </div>
+            </div>
+        );
     });
-
     return (
         <div className='feed-page-container' >
             <div className='search-bar'>
                 <label>Search Bar goes brrrrr</label>
-                <input 
-                type='text' 
-                placeholder='Search here'
-                onChange={(e) => setSearch(e.target.value)}
+                <input
+                    type='text'
+                    placeholder='Search here'
+                    onChange={(e) => setSearch(e.target.value)}
                 ></input>
             </div>
             <div className='add-drink-container' >
@@ -129,22 +157,18 @@ const DrinkFeed = () => {
 
                 </form>
 
-                    <button className='add-drink-button' onClick={handleAddDrinkButton}>
-                        Add drink to database! But decorate me too!
-                    </button>
-                
+                <button className='add-drink-button' onClick={handleAddDrinkButton}>
+                    Add drink to database! But decorate me too!
+                </button>
+
             </div>
-            <div className='drink-feed'>
-                <h2>This drink list comes from SingleDrink component</h2>
-                <SingleDrink/>
-                <SingleDrink/>
-                <SingleDrink/>
-            </div>
-            <div style={{backgroundColor:'#40ebd7',height:'100vh'}}>
+            
+            <div style={{ backgroundColor: '#40ebd7', height: '100vh' }}>
                 <h3>
                     This list will come from drinkTable with in DrinkFeed component
-                    <drinkTable/>
+
                 </h3>
+                {testTable}
             </div>
         </div>
     )
