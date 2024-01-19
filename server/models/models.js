ff46e011a2d49986aa03717776f77bde42ab4d72
LNRtopher <143825175+LNRtopher@users.noreply.github.com>
2024-01-18 20:25:44 -0800

    troubleshooting persisting state

--- server/models/models.js
+++ server/models/models.js
@@ -1,32 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// set URI from atlas:
-// const {DB_USERNAME, DB_PASSWORD} = process.env;
-// const MONGO_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@axolotl.xogzh1q.mongodb.net/?retryWrites=true&w=majority`;
+set URI from atlas:
+const {DB_USERNAME, DB_PASSWORD} = process.env;
+const MONGO_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@axolotl.xogzh1q.mongodb.net/?retryWrites=true&w=majority`;
 
-//connect to db:
-// mongoose.connect(MONGO_URI, {dbName: 'axolotl'})
-//     .then(() => console.log('Connected to Mongo DB.'))
-//     .catch(err => console.log(err));
+connect to db:
+mongoose.connect(MONGO_URI, {dbName: 'axolotl'})
+    .then(() => console.log('Connected to Mongo DB.'))
+    .catch(err => console.log(err));
 
-// // logic that will display in terminal if mongoose connection to atlas is successful
-// mongoose.connection.on('connected', () => {
-//     console.log('Mongoose is connected to Atlas!')
-// });
+// logic that will display in terminal if mongoose connection to atlas is successful
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected to Atlas!')
+});
 
-// // logic that will display in terminal if mongoose to atlas encounters an error
-// mongoose.connection.on('error', (err) => {
-//     console.log('Mongoose connection error: ', err)
-// });
+// logic that will display in terminal if mongoose to atlas encounters an error
+mongoose.connection.on('error', (err) => {
+    console.log('Mongoose connection error: ', err)
+});
 
-// // logic that will display in terminal if mongoose to atlas is disconnected
-// mongoose.connection.on('disconnected', () => {
-//     console.log('Mongoose disconnected.')
-// });
+// logic that will display in terminal if mongoose to atlas is disconnected
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected.')
+});
 
- //ATTEMPTED TO CLEAN UP MODELS PAGE TO ONLY HAVE MODEL/SCHEMA LOGIC
- //MOVED DB LOGIC TO SERVER.JS
+//  ATTEMPTED TO CLEAN UP MODELS PAGE TO ONLY HAVE MODEL/SCHEMA LOGIC
+//  MOVED DB LOGIC TO SERVER.JS
 
 // set schema for the 'drinks' collection:
 const drinkSchema = new Schema({
